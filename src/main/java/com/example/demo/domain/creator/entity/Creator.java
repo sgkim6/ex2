6b4dc654b27490a16fb82525c35a152f86fe282d
sgkim <kimsg64090@gmail.com>
2026-04-24 18:31:40 +0900

    feat: 강사 엔티티 추가

---
+++ src/main/java/com/example/demo/domain/creator/entity/Creator.java
@@ -0,0 +1,32 @@
+package com.example.demo.domain.creator.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "creators")
+@NoArgsConstructor
+public class Creator {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String externalId; // 외부 아이디(creator-1 ..)
+
+	@Column(nullable = false)
+	private String name;
+
+	public Creator(String externalId, String name) {
+		this.externalId = externalId;
+		this.name = name;
+	}
+}

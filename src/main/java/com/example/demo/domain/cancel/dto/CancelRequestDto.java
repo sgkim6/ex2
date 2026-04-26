fce202c57a0afdbbe37642c6c3f8d8d6fbc49d6b
sgkim <kimsg64090@gmail.com>
2026-04-27 01:00:46 +0900

    feat: 취소 요청 dto 추가

---
+++ src/main/java/com/example/demo/domain/cancel/dto/CancelRequestDto.java
@@ -0,0 +1,19 @@
+package com.example.demo.domain.cancel.dto;
+
+import java.time.OffsetDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CancelRequestDto {
+
+	//취소할 판매
+	private Long saleId;
+
+	//취소 금액
+	private Integer refundAmount;
+
+	//취소 일자
+	private OffsetDateTime canceledAt;
+}
